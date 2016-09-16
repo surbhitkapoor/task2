5e66b83bfe42ca9149aa4deee10a28084b591d77
Surbhit Kapoor <surbhitkapoor@gmail.com>
2016-09-16 18:24:49 +0530

    task2 lab3 first

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style></style>
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
+</head>
+<body>
+
+
+		<div class="container">
+			<div class="jumbotron">
+				<h1 id="h1" class="LoginForm">Login Form</h1>
+			</div>
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4">
+					<form name="LoginFrom">
+						<div class="form-group">
+							<label id="lable1" class="form-group" for="email">Email</label>
+							<input id="input1" class="form-control" type="email" name="">
+						</div>
+						<div class="form-group">
+							<label id="label2" class="form-group" for="password">Password</label>
+							<input id="input2" class="form-control" type="password" name="">
+						</div>
+						<div class="from-group">
+							<input id="input3" class="btn-primary form-control" type="submit" name="">
+						</div>
+						<div class="from-group">
+							<a id="lable3" class="brand" href="" ><center>Forgot Password</center></a>
+						</div>
+						<br>
+						<div class="from-group">
+							<label id="lable4" class="form-group">Don't have an account?</label>
+							<a id="lable5" class="form-group" href="">Sign Up</a>
+						</div>
+					</form>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+		</div>
+
+
+<!--
+    <h1>Login Form</h1>
+    <label id="emaillabel" class="loginform">Email</label>
+    <input type="email" name="email" id="email" class="loginform"><br>
+    <label id="passwordlabel" class="loginform">Password</label>
+    <input type="password" name="password" id="password" class="loginform"><br>
+    <input type="submit" value="Login" id="button" class="loginform" >  
+--> 
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<style></style>
+		<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
+	</head>
+	<body>
+
+		<div class="container">
+			<div class="jumbotron">
+				<h1 id="h1" class="SignUpForm">Sign Up</h1>
+			</div>
+			<div class="row">
+				<div class="col-md-4"></div>
+				<div class="col-md-4">
+					<form name="SignUpForm">
+						<div class="form-group">
+							<label id="lable1" class="form-group" for="email">Email</label>
+							<input id="input1" class="form-control" type="email" name="">
+						</div>
+						<div class="form-group">
+							<label id="lable2" class="form-group" for="username">Username</label>
+							<input id="input2" class="form-control" type="text" name="">
+						</div>
+						<div class="form-group">
+							<label id="lable3" class="form-group" for="password">Password</label>
+							<input id="input3" class="form-control" type="password" name="">
+						</div>
+						<div class="from-group">
+							<input id="input4" class="btn-success form-control" type="submit" name="">
+						</div>
+					</form>
+				</div>
+				<div class="col-md-4"></div>
+			</div>
+		</div>
+
+
+	</body>
+</html>
