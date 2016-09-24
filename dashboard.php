cfe59122cb2165da6c524f3141ce83a7362462bc
Surbhit Kapoor <surbhitkapoor@gmail.com>
2016-09-24 15:38:05 +0530

    as bootstrap was not linked

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<style></style>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	</head>
+
+	<body>
+		<div class="container">
+		</div>
+	</body>
+</html>
